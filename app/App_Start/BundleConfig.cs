e056c13f6c40565dcee9f05bbd1773ef6280af73
Marcin <walas.marcin@gmail.com>
2014-11-16 15:30:50 +0100

    First commit

---
+++ app/App_Start/BundleConfig.cs
@@ -0,0 +1,33 @@
+﻿using System.Web.Optimization;
+
+namespace app
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include("~/Content/scripts/jquery/jquery-{version}.js"));
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include("~/Content/scripts/jquery-validate/jquery.validate*"));
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include("~/Content/scripts/modernizr/modernizr-*"));
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                "~/Content/scripts/bootstrap/bootstrap.js",
+                "~/Content/scripts/respond.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/angular").Include(
+                "~/Content/scripts/angular/angular.js",
+                "~/Content/scripts/angular/angular-route.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/app").Include(
+                "~/Content/scripts/app/controllers.js"));
+
+
+
+            bundles.Add(new StyleBundle("~/bundles/styles").Include(
+                "~/Content/styles/bootstrap/bootstrap.css",
+                "~/Content/styles/site.css"));
+
+            //BundleTable.EnableOptimizations = true;
+        }
+    }
+}
---
+++ app/Content/tests/controllers-specs.js
@@ -0,0 +1,23 @@
+﻿///<reference path="~/Content/scripts/jasmine/jasmine.js"/>
+///<reference path="~/Content/scripts/angular/angular.js"/>
+///<reference path="~/Content/scripts/angular/angular-mocks.js"/>
+
+///<reference path="~/Content/scripts/app/controllers.js"/>
+
+
+/* jasmine specs for controllers go here */
+describe('PhoneCat controllers', function () {
+
+    describe('PhoneListCtrl', function () {
+
+        beforeEach(module('phonecatApp'));
+
+        it('should create "phones" model with 3 phones', inject(function ($controller) {
+            var scope = {},
+                ctrl = $controller('PhoneListCtrl', { $scope: scope });
+
+            expect(scope.phones.length).toBe(3);
+        }));
+
+    });
+});
